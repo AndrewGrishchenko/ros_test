91b76b9c49fe776d0c8fb464cac1a450f246391f
AndrewGrishchenko <andrew26080537@gmail.com>
2023-03-20 12:34:08 +0300

    bd1

--- build/mic_pub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ros_test/src/mic_pub
-# Build directory: /home/pi/ros_test/build/mic_pub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
